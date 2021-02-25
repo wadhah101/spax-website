2ab62bef8e3a4fb461a07f9b1c07f0168f614ef6
Wadhah Mahrouk <wadhah.mahroug15@gmail.com>
2021-02-25 19:59:13 +0100

    changed image

--- components/pages/challenge/Banner/index.tsx
+++ components/pages/challenge/Banner/index.tsx
@@ -1,21 +1,24 @@
 import * as React from 'react'
-import { FiDownload } from 'react-icons/fi'
+import { FiChevronRight, FiDownload } from 'react-icons/fi'
 
 const imageUrl = '/pages/challenge/banner.jpg'
 
 const ChallengeBanner: React.FunctionComponent = () => {
 	return (
 		<div className="relative flex flex-col min-h-screen ">
-			<img src={imageUrl} className="absolute top-0 left-0 w-full h-full" />
-			<div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50" />
+			<img
+				src={imageUrl}
+				className="absolute top-0 left-0 object-cover w-full h-full"
+			/>
+			<div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-70" />
 			<div className="relative z-10 flex items-center flex-grow w-full pb-20 text-white place-items-center mcontainer ">
 				<div className="flex flex-col items-center md:items-start">
 					<h1 className="text-4xl font-bold tracking-wide md:text-8xl">
 						CHALLENGE
 					</h1>
 					<h2 className="mt-6 text-2xl text-center md:text-left">
-						check our challenge Lorem ipsum dolor <br /> sit amet consectetur
-						adipisicing elit. Similique, nemo?
+						Our special challenge. <br /> The first of its kind in Tunisia IS
+						caling you
 					</h2>
 					<div className="flex ">
 						{/* TODO organize : repeating myself */}
@@ -27,6 +30,18 @@ const ChallengeBanner: React.FunctionComponent = () => {
 							<span>Download PDF</span>
 							<div className="ml-4 text-3xl ">
 								<FiDownload />
+							</div>
+						</a>
+
+						<a
+							className="inline-flex items-center px-6 py-2 mt-8 font-semibold tracking-wider text-black duration-300 bg-white border-2 border-white md:ml-4 md:text-xl text-opacity-70 "
+							href="https://ieeespaxinsatxenitsb.typeform.com/to/NcsOtEqu"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<span>INSCRIPTION</span>
+							<div className="ml-2 text-3xl ">
+								<FiChevronRight />
 							</div>
 						</a>
 					</div>
